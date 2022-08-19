7d26c5a556344508e412e08046af88743e1b3082
songhajang <angpan2004@gmail.com>
2022-08-19 15:01:32 +0900

    레이아웃 구성

---
+++ src/components/SectionContainer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Post :data="lst" v-for="(lst,key) in dataList" :key="key" />
+
+
+    </div>
+</template>
+
+<script>
+import Post from './ArticlePost.vue'
+export default {
+    components:{
+        Post,
+    },
+    props:{
+        dataList : Array,
+    }
+}
+</script>
+
+<style>
+</style>
